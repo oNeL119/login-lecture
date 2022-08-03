351e6dd6672e27330bba6e31999b8af4217c302b
LAPTOP-406BG4JB\kjy95 <jykim@hanvitsi.com>
2022-08-03 13:54:42 +0900

    nodemon

---
+++ app/app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+//모듈
+const express = require('express');
+const app = express();
+
+const home = require("./src/routes/home");
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+//라우팅
+app.use(express.static(`${__dirname}/src/public`));
+app.use("/", home); //use -> 미들 웨어를 등록해주는 메서드.
+
+
+module.exports = app;
+
+
+
+
+// HTTP로 서버 실행
+// const http = require('http');
+// const app = http.createServer((req, res) => {
+//     if(req.url === "/"){
+//         res.end("http root page");
+//     }else if(req.url === "/login"){
+//         res.end("login page");
+//     }
+// });
+
+// app.listen(3001, () => {
+//     console.log("http 서버 실행");
+// });
